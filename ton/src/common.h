f803b97739790314ab7acd8ebfb265bd0d88a1a1
tor4z <vwenjie@hotmail.com>
2016-03-25 15:54:34 -0400

    Add worker header

---
+++ ton/src/common.h
@@ -0,0 +1,15 @@
+#ifndef TON_COMMON_H_
+#define TON_COMMON_H_
+ 
+namespace ton {
+
+typedef enum status_e {
+    stopped,
+    running,
+    dead
+} Status;
+
+} 
+
+#endif //TON_COMMON_H_
+
---
+++ ton/src/worker.h
@@ -0,0 +1,21 @@
+#ifndef TON_WORKER_H_
+#define TON_WORKER_H_
+
+#include "common.h"
+
+namespace ton {
+
+class Worker {
+  public:
+    Worker();
+    ~Worker();
+  private:
+    Status status;
+}
+
+}
+
+
+#endif //TON_WORKER_H_
+
+ 
