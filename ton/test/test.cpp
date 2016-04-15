1a8e029ba5b2f03f3da46fc15fe1e37c43df6bea
ColeChan <chenwenjie27@gmail.com>
2016-04-15 18:21:50 +0800

    Update test

---
+++ ton/test/test.cpp
@@ -0,0 +1,13 @@
+/*
+    class TestTask: public Task {
+    }
+
+    Chan chan;
+    TestTask task = new TestTask();
+    Runner runner = Runner(chan);
+    runner.add(task);
+    
+    Msg msg;
+    msg.add("something");
+    chan.broadcast(msg);
+*/
