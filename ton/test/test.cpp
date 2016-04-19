de85fff0ab2acc837893ad36affe57bd78d09c91
ColeChan <chenwenjie27@gmail.com>
2016-04-19 14:20:13 +0800

    Update road map

--- ton/test/test.cpp
+++ ton/test/test.cpp
@@ -1,24 +1,6 @@
-/*
-    class TestTask: public Task {
-    }
-
-    Chan chan;
-    Worker worker;
-    Runner runner = Runner(chan);
-    runner.add(worker);
-    
-    TestTask task = new TestTask();
-    
-    Msg msg = new Msg();
-    msg.set_task(task);
-    chan.send(worker, msg);
-    chan.broadcast(msg);
-*/
-
 #include "common.h"
 #include "async.h"
 #include "test.h"
-
     
 int main() {
     test_f functions[] = { &test_async}
